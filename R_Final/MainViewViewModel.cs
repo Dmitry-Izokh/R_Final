f78d764dde2443fed2a46f5d8c175818bebe5be8
Дмитрий Изох <izokh_dv@mail.ru>
2023-02-23 14:36:56 +0300

    Добавьте файлы проекта.

---
+++ R_Final/MainViewViewModel.cs
@@ -0,0 +1,92 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R_Final
+{
+    public class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+        // Свойства
+        public DelegateCommand Command { get; }
+        private Document _doc;
+
+        public List<RoomTagType> RoomTegTypes { get; }/* = new List<RoomTagType>(null);*/
+        public RoomTagType SelectedRoomTegType { get; set; }
+        public List<Level> Levels { get; } /*= new List<Level>(null);*/
+        public Level SelectedLevel { get; set; }
+        public DelegateCommand SaveCommand { get; }      
+
+
+        //Конструктор для передачи сюда данных по текущему запущенному Revit
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+            _doc = _commandData.Application.ActiveUIDocument.Document;
+
+            RoomTegTypes = RoomTagTypesUtils.GetRoomTagType(commandData);
+            Levels = LevelUtils.GetLevels(commandData);                
+            SaveCommand = new DelegateCommand(OnSaveCommand);            
+        }
+        // Метод выполняющий программу
+        private void OnSaveCommand()
+        {
+            UIApplication uiapp = _commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            PlanTopology planTopology = doc.get_PlanTopology(SelectedLevel);
+            
+            using (var ts = new Transaction(doc, "Создание листов"))
+            {
+                ts.Start("Размещение марок помещений");
+                foreach (ElementId eid in planTopology.GetRoomIds())
+                {
+                    Room tmpRoom = doc.GetElement(eid) as Room;
+
+                    if (doc.GetElement(tmpRoom.LevelId) != null && tmpRoom.Location != null)
+                    {
+                        // Создайте указанный тип RoomTag, чтобы пометить комнату
+                        LocationPoint locationPoint = tmpRoom.Location as LocationPoint;
+                        UV point = new UV(locationPoint.Point.X, locationPoint.Point.Y);
+                        RoomTag newTag = doc.Create.NewRoomTag(new LinkElementId(tmpRoom.Id), point, null);
+                        newTag.RoomTagType = SelectedRoomTegType;
+
+                        List<RoomTag> tagListInTheRoom = new List<RoomTag>(newTag.Room.Id.IntegerValue);                        
+                        tagListInTheRoom.Add(newTag);
+                    }
+                }
+                ts.Commit();
+            }
+            RaiseCloseRequest();
+        }
+
+
+        //событие создается для скрытие окна на время выбора.
+        public event EventHandler HideRequest;
+        private void RaiseHideRequest()
+        {
+            HideRequest?.Invoke(this, EventArgs.Empty);
+        }
+        //событие создается для повторного открытия окна после отработки программы.
+        public event EventHandler ShowRequest;
+        private void RaiseShowRequest()
+        {
+            ShowRequest?.Invoke(this, EventArgs.Empty);
+        }
+        //событие создается для закрытия программы.
+        public event EventHandler CloseRequest;
+        private void RaiseCloseRequest()
+        {
+            CloseRequest?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ R_Final/RoomTagTypesUtils.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R_Final
+{
+    class RoomTagTypesUtils
+    {
+        public static List<RoomTagType> GetRoomTagType(ExternalCommandData commandData)
+        {
+            var doc = commandData.Application.ActiveUIDocument.Document;
+            List<RoomTagType> roomTagType = new FilteredElementCollector(doc)
+                .OfClass(typeof(FamilySymbol))
+                .OfCategory(BuiltInCategory.OST_RoomTags)
+                .Cast<RoomTagType>()
+                .ToList();
+            //Dictionary<int, List<RoomTag>> m_roomWithTags = new Dictionary<int, List<RoomTag>>();
+
+            //foreach (RoomTag roomTag in roomTagType)
+            //{
+            //    if (m_roomWithTags.ContainsKey(roomTag.Room.Id.IntegerValue))
+            //    {
+            //        List<RoomTag> tmpList = m_roomWithTags[roomTag.Room.Id.IntegerValue];
+            //        tmpList.Add(roomTag);
+            //    }
+            //}
+            return roomTagType;
+        }
+    }
+}
